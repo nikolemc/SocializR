f352fa2d825dfdd8d56cef62eca6b96912c79d03
Dat Tran <datc.tran@gmail.com>
2017-05-22 16:28:15 -0400

    xml export working. Working on RSS standards and refactoring now

---
+++ FinalProjectSocialzR/Controllers/MediaExportController.cs
@@ -0,0 +1,28 @@
+﻿using FinalProjectSocialzR.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FinalProjectSocialzR.Controllers
+{
+    public class MediaExportController : Controller
+    {
+        public ActionResult Index()
+        {
+            //XDocument doc = new XDocument(new XElement("body",
+            //                              new XElement("Message",
+            //                              new XElement("Content", "This is an email message"))));
+
+            ApplicationDbContext db = new ApplicationDbContext();
+
+            var mediaToExport = db.SavedSocialMessages.ToList();
+
+            Response.ContentType = "text/xml";
+            //Response.AppendHeader("Content-Disposition", String.Format("attachment;filename={0}", doc));
+
+            return View(mediaToExport);
+        }
+    }
+}
