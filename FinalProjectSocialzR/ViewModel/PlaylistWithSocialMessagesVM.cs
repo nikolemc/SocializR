8219fa9dca5164e651cc96b67759e55b7df82147
nikolemc <nikolemcstanley@gmail.com>
2017-05-25 17:52:39 -0400

    dragged message now has has a playlist associated to it

--- FinalProjectSocialzR/ViewModel/PlaylistWithSocialMessagesVM.cs
+++ FinalProjectSocialzR/ViewModel/PlaylistWithSocialMessagesVM.cs
@@ -1,11 +1,5 @@
-﻿using System;
+﻿using FinalProjectSocialzR.Models;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Data.SqlClient;
-using System.ComponentModel;
-using System.ComponentModel.DataAnnotations;
-using FinalProjectSocialzR.Models;
 
 namespace FinalProjectSocialzR.ViewModel
 {
@@ -13,33 +7,11 @@ namespace FinalProjectSocialzR.ViewModel
     {
         
         public int Id { get; set; }
+        public string SelectedPlayListName { get; set; }
 
-        public string PlaylistName { get; set; }
+        public IEnumerable<Playlist> AllPlaylist { get; set; }
+    
         public IEnumerable<SavedSocialMessage> Messages { get; set; }
-        //determines which social source: twitter, instagram
-        //public string Source { get; set; }
-
-        //[DisplayName("Avatar")]
-        //[DataType(DataType.ImageUrl)]
-        //public string ImageUrl { get; set; }
-
-        //[DisplayName("Screen Name")]
-        //public string ScreenName { get; set; }
-
-        ////This is the copied message that may have been edited
-        //[DisplayName("SocialMessage")]
-        //public string Text { get; set; }
-        
-        //public DateTime PostTimeStamp { get; set; }
-
-        //// for direct twitter post
-        //public string PostContentUrl { get; set; }
-
-        //public string UserName { get; set; }
-
-
-        
-
-
+    
     }
 }
