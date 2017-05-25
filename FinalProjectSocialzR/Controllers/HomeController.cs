1a7dafb8941093978b30b5cb3743d2730b8fec1f
Dat Tran <datc.tran@gmail.com>
2017-05-25 18:00:04 -0400

    still working on sesearch params beyond base twitter params

--- FinalProjectSocialzR/Controllers/HomeController.cs
+++ FinalProjectSocialzR/Controllers/HomeController.cs
@@ -20,7 +20,6 @@ namespace FinalProjectSocialzR.Controllers
                 var userId = User.Identity.GetUserId();
                 var userName = User.Identity.GetUserName();
             }
-
             //var vm = new ApplicationDbContext().Tweet.OrderByDescending(o => o.PostedTimeStamp).Include(i => i.message).ToList();
             //return View(vm);
             return View();
@@ -46,104 +45,7 @@ namespace FinalProjectSocialzR.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
-        }
-
-
-        [ActionName("CustomSearch")]
-        public async Task<ActionResult> CustomSearchAsync(string searchTerm)
-        {
-
-            var auth = new MvcAuthorizer
-            {
-                CredentialStore = new SessionStateCredentialStore()
-            };
-
-            var ctx = new TwitterContext(auth);
-
-            var searchResponse =
-                 await
-                 (from search in ctx.Search
-                  where search.Type == SearchType.Search &&
-                        search.Query == ("\"" + searchTerm + "\"")
-                  select search)
-                 .SingleOrDefaultAsync();
-
-            var tweets = new List<Tweet>();
-
-            foreach (var item in searchResponse.Statuses)
-            {
-                if (item.ExtendedEntities.MediaEntities.Count != 0)
-                {
-                    var tweetToAdd = new Tweet
-                    {
-                        ImageUrl = item.User.ProfileImageUrl,
-                        ScreenName = item.User.ScreenNameResponse,
-                        Text = item.Text,
-                        PostTimeStamp = item.CreatedAt,
-                        Media = item.ExtendedEntities.MediaEntities.FirstOrDefault(f => f.ExpandedUrl != null).ExpandedUrl.ToString(),
-                        MediaImage = item.ExtendedEntities.MediaEntities.FirstOrDefault(f => f.ExpandedUrl != null).MediaUrl.ToString()
-                    };
-                    tweets.Add(tweetToAdd);
-                }
-                else
-                {
-                    var tweetToAdd = new Tweet
-                    {
-                        ImageUrl = item.User.ProfileImageUrl,
-                        ScreenName = item.User.ScreenNameResponse,
-                        Text = item.Text,
-                        PostTimeStamp = item.CreatedAt
-                    };
-                    tweets.Add(tweetToAdd);
-                }           
-                
-            }
-
-            return PartialView("_TwitterSearchResultsPartial", tweets);
-        }
-
-        [ActionName("CustomSearchZip")]
-        public async Task<ActionResult> CustomSearchZipAsync(string searchTermZip = "trump", string lati = "27.782254", string longi = "-82.667619", string radi = "1")
-        {
-
-            var auth = new MvcAuthorizer
-            {
-                CredentialStore = new SessionStateCredentialStore()
-            };
-
-            var ctx = new TwitterContext(auth);
-
-            var searchResponse =
-                 await
-                 (from search in ctx.Search
-                  where search.Type == SearchType.Search &&
-                        search.GeoCode == (lati + "," + longi + "," + radi + "mi") &&
-                        search.Query == ("\"" + searchTermZip + "\"") &&
-                        search.Count == 100
-                  select search)
-                 .SingleOrDefaultAsync();
-
-            var tweets = new List<Tweet>();
-
-            foreach (var item in searchResponse.Statuses)
-            {
-                var tweetToAdd = new Tweet
-                {
-                    ImageUrl = item.User.ProfileImageUrl,
-                    ScreenName = item.User.ScreenNameResponse,
-                    Text = item.Text,
-                    PostTimeStamp = item.CreatedAt,
-                    UserName = item.User.Name,
-                    PostContentUrl = item.OEmbedUrl,
-                    Media = item.ExtendedEntities.MediaEntities.FirstOrDefault(f => f.DisplayUrl != null).ToString()
-                };
-
-
-                tweets.Add(tweetToAdd);
-            }
-
-            return View(tweets);
-        }
+        }        
     }
 }
 
