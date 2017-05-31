9c35a637eed381e2be743b9a1428e6f200807d83
Dat Tran <datc.tran@gmail.com>
2017-05-30 21:14:15 -0400

    commit for merge

--- FinalProjectSocialzR/Scripts/sortablelist.js
+++ FinalProjectSocialzR/Scripts/sortablelist.js
@@ -99,3 +99,114 @@ $(function () {
         //    alert("sender = " + ui.sender[0].id); // Where it came from
         //    alert("item = " + ui.item[0].innerHTML); //Which item (or ui.item[0].id)
         //}
+
+
+///modal stuff
+
+//$(function () {
+//    // there's the gallery and the transfer
+//    var $twitterSearchDisplayFront = $("#twitterSearchDisplayFront"),
+//        $playlistSearchDisplayFront = $("#playlistSearchDisplayFront"),
+
+//    // let the gallery items be draggable
+//        $("div", $twitterSearchDisplayFront).draggable({
+//        cancel: "a.ui-icon", // clicking an icon won't initiate dragging
+//        revert: "invalid", // when not dropped, the item will revert back to its initial position
+//        containment: "document",
+//        helper: "clone",
+//        cursor: "move"
+//    });
+
+//    // let the transfer be droppable, accepting the gallery items
+//    $playlistSearchDisplayFront.droppable({
+//        accept: "#twitterSearchDisplayFront > div",
+//        activeClass: "ui-state-highlight",
+//        drop: function (event, ui) {
+//            deleteImage(ui.draggable);
+//        }
+//    });
+
+//    // let the gallery be droppable as well, accepting items from the transfer
+//    $twitterSearchDisplayFront.droppable({
+//        accept: "#playlistSearchDisplayFront div",
+//        activeClass: "custom-state-active",
+//        drop: function (event, ui) {
+//            recycleImage(ui.draggable);
+//        }
+//    });
+    
+//    // function for generating info of icon/s in drop box
+//    $('button.generate').click(function () {
+//        var content = $('div div h5', $playlistSearchDisplayFront).map(function (i, v) {
+//            return $(this).text();
+//        }).get();
+//        $gen_dialog.find('.diag-content').html(content.join(', ')).end().dialog('open');
+//    });
+//    //function for resetting the icons back to original positions
+//    $('button.reset').click(function () {
+//        $('div div', $playlistSearchDisplayFront).each(function () {
+//            recycleImage($(this));
+//        });
+//    });
+
+//    toggleButtons();
+
+//    // image deletion function
+//    var recycle_icon = "<a href='link/to/recycle/script/when/we/have/js/off' title='Transfer this icon back' class='ui-icon ui-icon-transfer-e-w'>Transfer this icon back</a>";
+//    function deleteImage($item) {
+//        $item.fadeOut(function () {
+//            var $list = $("div", $playlistSearchDisplayFront).length ?
+//                $("div", $playlistSearchDisplayFront) :
+//                $("<div class='twitterSearchDisplayFront ui-helper-reset'/>").appendTo($playlistSearchDisplayFront);
+
+//            $item.find("a.ui-icon-transferthick-e-w").remove();
+//            $item.append(recycle_icon).appendTo($list).fadeIn(function () {
+//                $item
+//                    .animate({ width: "48px" })
+//                    .find("img")
+//                    .animate({ height: "36px" }, function () {
+//                        toggleButtons();
+//                    });
+//            });
+//        });
+//    }
+
+//    // image recycle function
+//    var transfer_icon = "<a href='link/to/transfer/script/when/we/have/js/off' title='Transfer Across' class='ui-icon ui-icon-transferthick-e-w'>Transfer Across</a>";
+//    function recycleImage($item) {
+//        $item.fadeOut(function () {
+//            $item
+//                .find("a.ui-icon-transfer-e-w")
+//                .remove()
+//                .end()
+//                .css("width", "96px")
+//                .append(transfer_icon)
+//                .find("img")
+//                .css("height", "72px")
+//                .end()
+//                .appendTo($twitterSearchDisplayFront)
+//                .fadeIn(function () {
+//                    toggleButtons();
+//                });
+//        });
+//    }
+//    // display buttons when icon transferred across     
+//    function toggleButtons() {
+//        $('div.col3 button').toggle($('> div > div', $playlistSearchDisplayFront).length > 0);
+//    }
+    
+//    // resolve the icons behavior with event delegation
+//    $("div.twitterSearchDisplayFront > div").click(function (event) {
+//        var $item = $(this),
+//            $target = $(event.target);
+
+//        if ($target.is("a.ui-icon-transferthick-e-w")) {
+//            deleteImage($item);
+//        } else if ($target.is("a.ui-icon-transfer-e-w")) {
+//            recycleImage($item);
+//        }
+
+//        return false;
+//    });
+//});
+
