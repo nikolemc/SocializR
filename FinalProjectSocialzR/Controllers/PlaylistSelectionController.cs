e1e29c5cc43d0c722a8bce6af021e70d2d68846e
nikolemc <nikolemcstanley@gmail.com>
2017-05-30 20:36:32 -0400

    working on add new playlist. button and modal open, now need to make dynamic

--- FinalProjectSocialzR/Controllers/PlaylistSelectionController.cs
+++ FinalProjectSocialzR/Controllers/PlaylistSelectionController.cs
@@ -13,11 +13,12 @@ namespace FinalProjectSocialzR.Controllers
     {
         private ApplicationDbContext db = new ApplicationDbContext();
         // GET: PlaylistChoice
-        public ActionResult GetPlaylist( int id)
+        public ActionResult GetPlaylist(int id)
         {
-            var rv = db.Playlists.Select(s => new { Playlist = s, Message = s.SavedSocialMessage,  }).First(f => f.Playlist.Id == id); 
+            var rv = db.Playlists.Select(s => new { Playlist = s, Message = s.SavedSocialMessage, }).First(f => f.Playlist.Id == id);
             var playLists = db.Playlists.ToList();
-            var vm = new PlaylistWithSocialMessagesVM {
+            var vm = new PlaylistWithSocialMessagesVM
+            {
                 Messages = rv.Message,
                 PlayListName = rv.Playlist.PlaylistName,
                 AllPlaylist = playLists,
@@ -34,15 +35,31 @@ namespace FinalProjectSocialzR.Controllers
             db.Entry(playlist).State = EntityState.Modified;
             db.SaveChanges();
 
-            var rv = db.Playlists.Select(s => new { Playlist = s, }).First(f => f.Playlist.Id == id); 
+            var rv = db.Playlists.Select(s => new { Playlist = s, }).First(f => f.Playlist.Id == id);
             var playLists = db.Playlists.ToList();
             var vm = new PlaylistWithSocialMessagesVM
             {
                 Id = id,
-                PlayListName = rv.Playlist.PlaylistName,   
+                PlayListName = rv.Playlist.PlaylistName,
                 AllPlaylist = db.Playlists.ToList()
 
             };
+
+            return PartialView("_PlaylistPartial", vm);
+        }
+
+        [HttpPost]
+        public ActionResult Create([Bind(Include =
+        "Id,PlaylistName,LastModifiedTimeStamp")] Playlist playlist)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Playlists.Add(playlist);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            var vm = new PlaylistWithSocialMessagesVM { };
 
             return PartialView("_PlaylistPartial", vm);
         }
@@ -57,39 +74,23 @@ namespace FinalProjectSocialzR.Controllers
             {
                 db.SavedSocialMessages.Remove(item);
             }
-         
+
 
             db.Entry(playlist).State = EntityState.Modified;
             db.Playlists.Remove(playlist);
             db.SaveChanges();
 
-            var vm = new PlaylistWithSocialMessagesVM {};
+            var vm = new PlaylistWithSocialMessagesVM { };
 
             return PartialView("_PlaylistPartial", vm);
-        }
-
-        [HttpGet]
-        public ActionResult Delete2(int id, int playListId)
-        {
-            SavedSocialMessage savedSocialMessage = db.SavedSocialMessages.Find(id);
-            db.SavedSocialMessages.Remove(savedSocialMessage);
-            db.SaveChanges();
-
-            var rv = db.Playlists.Select(s => new { Playlist = s, Message = s.SavedSocialMessage, }).First(f => f.Playlist.Id == playListId);
-            var playLists = db.Playlists.ToList();
-            var vm = new PlaylistWithSocialMessagesVM
-            {
-                Messages = rv.Message,
-                PlayListName = rv.Playlist.PlaylistName,
-                AllPlaylist = playLists,
-                Id = id
-            };
-
-            return PartialView("_PlaylistSearchResultsPartial", vm);
         }
 
 
 
     }
 
+
+
+
 }
+
