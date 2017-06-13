7e93d04d6f836e212055ff8341e9a02d6dd5d0bc
nikolemc <nikolemcstanley@gmail.com>
2017-06-13 13:11:25 -0400

    updated style of message. Fixed edit text box to have multiplelines

--- FinalProjectSocialzR/Scripts/formatdatetime.js
+++ FinalProjectSocialzR/Scripts/formatdatetime.js
@@ -2,7 +2,7 @@
     console.log("loaded")
     $(".time").each(function () {
         console.log("formatid", this)
-        let _formattedDate = moment($(this).html()).format("MMMM Do YYYY, h:mm:ss a");
+        let _formattedDate = moment($(this).html()).format("ddd MMMM YYYY, h:mm:ss a");
         //let _formattedDate = moment($(this).html()).startOf("minute").fromNow();
         $(this).html(_formattedDate);
     });
